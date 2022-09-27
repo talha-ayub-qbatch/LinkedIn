b8a397d4b167cdc40839ec2478e26003b3e5c45b
talha-ayub-qbatch <talha.ayub@qbatch.com>
2022-09-27 16:38:36 +0500

    Change in reqss

--- my_project/settings.py
+++ my_project/settings.py
@@ -88,16 +88,16 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'test',
+#         'USER': 'postgres',
+#         'PASSWORD': '123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 import dj_database_url
 DATABASES = {'default': dj_database_url.config()}
