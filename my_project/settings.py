4b1e17ee5224fa37a6698f4bcd6fabc571c669a0
talha-ayub-qbatch <talha.ayub@qbatch.com>
2022-09-27 17:33:47 +0500

    Adding static file

--- my_project/settings.py
+++ my_project/settings.py
@@ -161,7 +161,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-            BASE_DIR,'static',
+            BASE_DIR,'static/images',
                     ]
 
 
