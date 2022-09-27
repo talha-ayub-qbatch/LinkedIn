de3987bef80bbe6252308d71486722f530e45a2b
talha-ayub-qbatch <talha.ayub@qbatch.com>
2022-09-27 17:39:06 +0500

    Adding static file

--- my_project/settings.py
+++ my_project/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'my_project.urls'
